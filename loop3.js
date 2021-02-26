2bda9ee8996f2034a36ecca235ebfce0bae36d25
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-26 15:12:53 +0100

    loop3

--- loop3.js
+++ loop3.js
@@ -6,4 +6,4 @@ for(let i = 0; i < tab.length; i++){
     console.log(tab[i])
 }
 
- console.log(tab.join(" / "))       //Solution sans la boucle for
+ console.log(tab.join(" / "))       //Solution sans la boucle for 
