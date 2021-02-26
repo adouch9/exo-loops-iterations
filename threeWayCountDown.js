57daa2fb8ea09e63c108cb59a753575a6a95d9ff
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-26 15:52:06 +0100

    threeWay

---
+++ threeWayCountDown.js
@@ -0,0 +1,36 @@
+for(let i = 101; i >= 1; i -= 2){
+    console.log(i)
+  }
+ 
+
+
+
+
+let tab = []
+let i = 101;
+
+  while(i >= 1){
+    console.log(i)
+    tab.push(i)
+    i -= 2
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   for(let i = 1; i <= 101; i+=2){
+     
+
+//     console.log(i)
+// }
